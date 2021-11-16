3f1e52bd242e823578dc6e27b9eb42b3302d4439
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-11-16 20:54:15 +0530

    Connected to local database using driverclass

---
+++ ConnectToDatabase.java
@@ -0,0 +1,30 @@
+package bridgelabzAddressBook;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+import com.mysql.cj.jdbc.Driver;
+public class ConnectToDatabase {
+   public static void main(String[] args) {
+      Connection con = null;
+      try {
+    	  Class.forName("com.mysql.cj.jdbc.Driver");
+    	  System.out.println("Driver loaded");
+         con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user = root & password = Mathi@30 ");
+         if (con != null) {
+            System.out.println("Database connection is successful !!!!");
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+  private static void listDrivers() {
+	  Enumeration<java.sql.Driver> driverList = DriverManager.getDrivers();
+	  while (driverList.hasMoreElements()) {
+		  Driver driverClass =(Driver) driverList.nextElement();
+		  System.out.println("  "+driverClass.getClass().getName());
+	  }
+  } 
+ }
+ 
